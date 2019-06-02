6be6a3fe2ff8d8f68b56bf128e6d46e6b0cc456f
Amanda <amandalragone@gmail.com>
2019-06-01 21:41:17 -0500

    Created server.js.

--- server.js
+++ server.js
@@ -7,3 +7,8 @@ const app = express();
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+app.listen(PORT, function() {
+    console.log("App listening on PORT " + PORT);
+});
+  
